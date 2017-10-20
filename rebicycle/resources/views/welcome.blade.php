1b636a1d03d97c4058fb5b428e14e7e4077748df
ajkea <hendrikvd@msn.com>
2017-10-20 17:31:45 +0200

    add hero image lay out and buttons on hero image

--- rebicycle/resources/views/welcome.blade.php
+++ rebicycle/resources/views/welcome.blade.php
@@ -3,16 +3,39 @@
 </div>
 <div class="intro">
     <div class="hero-image">
-        <h1>Rebicycle</h1>
+        <h1 class="scroll-object">Rebicycle</h1>
+        <div class="buttons-hero-image">
+            <div class="left col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                <button type="button" class="btn btn-outline-primary btn-lg">Kopen</button>
+            </div>
+            <div class="right col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                <button type="button" class="btn btn-outline btn-lg">Verkopen</button>
+            </div>
+        </div>
+
         <i style="font-size:2em;" class="fal fa-chevron-double-down"></i>
     </div>
-    <!-- <h1>nog wat content verder op de pagina</h1> -->
-    <h2>en nog meer content maar kleiner</h2>
-            <i style="font-size:2em;" class="fas fa-chevron-double-down fa-6x"></i>
-    <p>nu gewoon saaie tekst bla bla duurt lang</p>
-    <!-- <h1>wow grote titel nutteloos</h1> -->
-    <h3>klein klein klein</h3>
-    <p>tekst last but not least</p>
 </div>
 <div class="container">
+    <h2>en nog meer content maar kleiner</h2>
+    <i style="font-size:2em;" class="fas fa-chevron-double-down fa-6x"></i>
+    <p>nu gewoon saaie tekst bla bla duurt lang</p>
+    <h1>wow grote titel nutteloos</h1>
+    <h3>klein klein klein</h3>
+    <p>tekst last but not least</p>
+    <<p>test</p><h1>wow grote titel nutteloos</h1>
+    <h3>klein klein klein</h3>
+    <p>tekst last but not least</p>
+    <<p>test</p><h1>wow grote titel nutteloos</h1>
+    <h3>klein klein klein</h3>
+    <p>tekst last but not least</p>
+    <<p>test</p><h1>wow grote titel nutteloos</h1>
+    <h3>klein klein klein</h3>
+    <p>tekst last but not least</p>
+    <<p>test</p>
+    <<p>test</p><h1>wow grote titel nutteloos</h1>
+    <h3>klein klein klein</h3>
+    <p>tekst last but not least</p>
+    <<p>test</p>
+
 @endsection
