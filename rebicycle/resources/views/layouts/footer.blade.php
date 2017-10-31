aa7c2405bc2587a8f8b062a169103ff371370b0d
ajkea <hendrikvd@msn.com>
2017-10-31 19:47:27 +0100

    edit footer

--- rebicycle/resources/views/layouts/footer.blade.php
+++ rebicycle/resources/views/layouts/footer.blade.php
@@ -1,21 +1,22 @@
 <div class="footer">
     <div class="container">
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+        <div class="col-xs-8 col-sm-6 col-md-6 col-lg-6 col-xl-6">
             <p>Geef je email in om als eerste de nieuwste fietsen te ontdekken.</p>
             <div class="input-group">
-                <input type="text" class="form-control" placeholder="Search for..." aria-label="Search for...">
+                <input type="text" class="form-control" placeholder="e-mail" aria-label="e-mail">
                 <span class="input-group-btn">
-                    <button class="btn btn-primary" type="button">Go!</button>
+                    <button class="btn btn-primary" type="button">Inschrijven</button>
                 </span>
             </div>
         </div>
-        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 col-xl-6">
-            <h1>Rebicycle</h1>
-            <h3>Contact:</h3>
-            <p>Groenhoflei 23</p>
-            <p>2000 Antwerpen</p>
-            <p>+32 (0)4 98 59 95 65</p>
-            <p>info@rebicycle.be</p>
+        <div class="contact col-xs-4 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+            <h3><i class="fas fa-address-card"></i> Contact:</h3>
+            <ul>
+                <li>Groenhoflei 31</li>
+                <li>2000 Antwerpen</li>
+                <li>+32 4 98 53 95 65</li>
+                <li>info@rebicycle.com</li>
+            </ul>
         </div>
     </div>
 </div>
