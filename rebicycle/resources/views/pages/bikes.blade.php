c3cecbefe2e72e2443ffe54fc7d29cbd10b5c4fe
ajkea <hendrikvd@msn.com>
2017-11-10 23:26:46 +0100

    update layout bikes

--- rebicycle/resources/views/pages/bikes.blade.php
+++ rebicycle/resources/views/pages/bikes.blade.php
@@ -169,7 +169,7 @@
     </form>
     </div>
     <div class="col-xs12 col-sm-9 col-md-9">
-        <div class="bicycles">
+        <div class="bicycles bikes">
             @foreach($allBikes as $key => $bike)
             <a href="/bike/{{ $bike->bike_id }}">
             <div class="bikeSale col-xs-6 col-sm-6 col-md-4" id="bike">
